45c4e34157f6de14f067322b5b5a59a8706ef92f
Le Hai Trieu <trieulehai66@gmail.com>
2026-01-13 16:27:45 +0700

    Hoc sinh nao nop bai nay

--- src/main/java/com/chrollo_dev/EduSentinel/modules/submission/repository/SubmissionRepository.java
+++ src/main/java/com/chrollo_dev/EduSentinel/modules/submission/repository/SubmissionRepository.java
@@ -9,4 +9,5 @@ import java.util.List;
 @Repository
 public interface SubmissionRepository  extends JpaRepository<Submission, String> {
     List<Submission> findAllByStudent_UsernameOrderByCreateAtDesc(String username);
+    List<Submission> findAllByHomeWork_IdOrderByScoreDesc(String homeworkId);
 }
