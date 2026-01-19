6b019eb214bc4ce0903451c7746c4f8b6cbe1e68
Le Hai Trieu <trieulehai66@gmail.com>
2026-01-19 15:51:42 +0700

    Fix cors

--- src/main/java/com/chrollo_dev/EduSentinel/config/security/SecurityConfig.java
+++ src/main/java/com/chrollo_dev/EduSentinel/config/security/SecurityConfig.java
@@ -54,7 +54,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         // Cho phép Frontend (React) truy cập
-        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedOriginPatterns(Arrays.asList("*"));
 
         // Cho phép các method
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
